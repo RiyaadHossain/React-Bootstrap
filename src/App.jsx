c7d2f427d8d011b97f93d79722659dd63e3d590c
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-28 14:23:42 +0600

    Bootstrap Card Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Button } from 'react-bootstrap';
 import './App.css';
 import Buttons from './Components/Buttons/Buttons';
+import Cards from './Components/Card Group/Cards';
 
 function App() {
   return (
     <div className="App">
       <h1 className='my-5'>Hello React Bootstrap</h1>
       <Button className='my-3' variant="primary">Primary</Button>
-      <Buttons/>
+      <Buttons />
+      <Cards/>
     </div>
   );
 }
---
+++ src/Components/Card Group/Cards.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button, Card, CardGroup } from "react-bootstrap";
+import Cardd from "../Card/Card";
+// import Cardd from "../Card/Card";
+
+const Cards = () => {
+  return (
+    <div>
+      <CardGroup>
+        <Cardd />
+        <Cardd />
+        <Cardd />
+      </CardGroup>
+    </div>
+  );
+};
+
+export default Cards;
+
+// <Card/>
+// <Card/>
+// <Card/>
