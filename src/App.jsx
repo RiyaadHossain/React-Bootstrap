92e17052e919eef946c80e614a6acc9afcc2b56f
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-28 14:10:25 +0600

    Buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
+import Buttons from './Components/Buttons/Buttons';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='my-5'>Hello React Bootstrap</h1>
+      <Button className='my-3' variant="primary">Primary</Button>
+      <Buttons/>
     </div>
   );
 }
